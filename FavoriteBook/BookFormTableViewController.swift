f3db27a1ae3601d71a3b27a29a22e99c732dd12f
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-05-14 22:13:58 +0300

    Create a static table view
    Create: BookFormTableViewController

---
+++ FavoriteBook/BookFormTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BookFormTableViewController.swift
+//  FavoriteBooks
+//
+//  Created by Office-iMac on 2020-05-14.
+//
+
+import UIKit
+
+class BookFormTableViewController: UITableViewController {
+
+    
+//    MARK: - StartHere:
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
